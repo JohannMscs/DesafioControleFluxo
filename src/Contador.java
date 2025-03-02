90109d382f5fd2cdf3fbab8773a0639605dde503
JohannMscs <johann.moscoso@gmail.com>
2025-03-02 17:05:46 -0300

    Conclusão do desafio

--- src/Contador.java
+++ src/Contador.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+            int resultadoSubtracaoParametroUmEDois = parametroUm - parametroDois;
+            for (int impressaoOcorrencias = 1; impressaoOcorrencias <= resultadoSubtracaoParametroUmEDois; impressaoOcorrencias++){
+                System.out.println("Imprimindo o número " + impressaoOcorrencias);
+            }
+
+        }catch (ParametrosInvalidosException e){
+            System.out.println("ERROR: O Segundo número precisa ser menor que o primeiro");
+        }
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if(parametroUm < parametroDois){
+            throw  new ParametrosInvalidosException();
+        }
+    }
 }
